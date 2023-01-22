2b275892c88446745a39279e666e75062234842e
André Filipe Barranco <b.andrefilipe@gmail.com>
2023-01-22 14:14:32 -0300

    client: add jackson databind dependency

--- client/client-http/build.gradle.kts
+++ client/client-http/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     implementation(project(":application"))
 
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
