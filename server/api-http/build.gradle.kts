bb39b9d4be6e1a097f34062de97ce5b379be3948
André Filipe Barranco <b.andrefilipe@gmail.com>
2023-01-22 08:28:23 -0300

    server: add spring web dependencies

---
+++ server/api-http/build.gradle.kts
@@ -0,0 +1,8 @@
+dependencies {
+    implementation(project(":application"))
+
+    // Spring Boot Starter Web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    // Jackson Module: Kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+}
